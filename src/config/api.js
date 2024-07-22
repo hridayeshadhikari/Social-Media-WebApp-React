4038cead4a1891d3a13a11c079ede0593ff7e163
Hridayesh Adhikari <140496685+hridayeshadhikari@users.noreply.github.com>
2024-07-22 17:54:21 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 //url in which backend application is running
 // export const API_BASE_URL="https://social-media-webapp-production.up.railway.app"
-export const API_BASE_URL="http://localhost:8080"
+export const API_BASE_URL="https://adequate-acceptance-production.up.railway.app"
 
 const jwtToken=localStorage.getItem("jwt")
 
